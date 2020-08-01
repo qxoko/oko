e128b3a56ce94e40d62896be3deb7a086f30695b
Harley Denham <45082543+qxoko@users.noreply.github.com>
2020-08-01 20:03:54 +0100

    "keyword" syntax category (forgotten)

--- syntax_highlight.go
+++ syntax_highlight.go
@@ -17,6 +17,7 @@ type Highlighter_Data struct {
 	String  []string
 	Entity  []string
 	Builtin []string
+	Keyword []string
 	Number  []string
 	Boolean []string
 	Comment []string
@@ -28,6 +29,7 @@ type Highlighter struct {
 	String  []*regexp.Regexp
 	Entity  []*regexp.Regexp
 	Builtin []*regexp.Regexp
+	Keyword []*regexp.Regexp
 	Number  []*regexp.Regexp
 	Boolean []*regexp.Regexp
 	Comment []*regexp.Regexp
@@ -63,6 +65,9 @@ func load_syntax(name string) *Highlighter {
 	for _, d := range data.Builtin {
 		compiled.Builtin = append(compiled.Builtin, regexp.MustCompile(d))
 	}
+	for _, d := range data.Keyword {
+		compiled.Keyword = append(compiled.Keyword, regexp.MustCompile(d))
+	}
 	for _, d := range data.Number {
 		compiled.Number = append(compiled.Number, regexp.MustCompile(d))
 	}
@@ -96,6 +101,9 @@ func highlight_code(text, hlight string) string {
 		}
 		for _, r := range h.Builtin {
 			line = r.ReplaceAll(line, []byte(`<span class='token builtin'>$1</span>`))
+		}
+		for _, r := range h.Keyword {
+			line = r.ReplaceAll(line, []byte(`<span class='token keyword'>$1</span>`))
 		}
 		for _, r := range h.Number {
 			line = r.ReplaceAll(line, []byte(`<span class='token number'>$1</span>`))
